8a17989ce092c626fbc3c039a637f17ab3be5e92
Matthew Oltendorf <matthew@oltendorf.net>
2016-05-09 11:42:03 -0700

    Use backend data in specials tab

--- app/src/main/java/io/mdx/app/menu/fragment/SpecialsFragment.java
+++ app/src/main/java/io/mdx/app/menu/fragment/SpecialsFragment.java
@@ -8,9 +8,15 @@ import android.view.View;
 
 import net.moltendorf.android.recyclerviewadapter.RecyclerViewAdapter;
 
-
 import io.mdx.app.menu.R;
+import io.mdx.app.menu.model.Specials;
+import io.mdx.app.menu.network.Backend;
 import io.mdx.app.menu.viewholder.SpecialViewHolder;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 /**
  * Created by moltendorf on 16/4/29.
@@ -28,16 +34,49 @@ public class SpecialsFragment extends BaseFragment {
   }
 
   @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    createAdapter();
+    fetchSpecials();
+  }
+
+  @Override
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
     setupList();
   }
 
-  private void setupList() {
+  private void createAdapter() {
     specialsAdapter = new RecyclerViewAdapter(getContext());
     specialsAdapter.setViewHolders(SpecialViewHolder.class);
+  }
 
+  private void fetchSpecials() {
+    Observable<Specials> observable = Backend.getService().getSpecials();
+
+    observable.subscribeOn(Schedulers.newThread())
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribe(new Subscriber<Specials>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+          Timber.e(e.getMessage());
+        }
+
+        @Override
+        public void onNext(Specials specials) {
+          specialsAdapter.changeDataSet(specials.getItems());
+        }
+      });
+  }
+
+  private void setupList() {
     list = (RecyclerView) getView();
     list.setLayoutManager(new LinearLayoutManager(getContext()));
     list.setAdapter(specialsAdapter);
