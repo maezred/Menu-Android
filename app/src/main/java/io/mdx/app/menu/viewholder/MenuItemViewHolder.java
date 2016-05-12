6cff1b773545d42cbb3638991cacaa933dd3fc40
Matthew Oltendorf <matthew@oltendorf.net>
2016-05-12 16:33:50 -0700

    Hook up favorites storage to menu tab

--- app/src/main/java/io/mdx/app/menu/viewholder/MenuItemViewHolder.java
+++ app/src/main/java/io/mdx/app/menu/viewholder/MenuItemViewHolder.java
@@ -2,15 +2,19 @@ package io.mdx.app.menu.viewholder;
 
 import android.content.Context;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.squareup.picasso.Picasso;
 
 import net.moltendorf.android.recyclerviewadapter.RecyclerViewAdapter;
 
 import io.mdx.app.menu.R;
+import io.mdx.app.menu.data.Database;
 import io.mdx.app.menu.model.MenuItem;
+import rx.functions.Action1;
 
 /**
  * Created by moltendorf on 16/5/9.
@@ -18,6 +22,21 @@ import io.mdx.app.menu.model.MenuItem;
 public class MenuItemViewHolder extends RecyclerViewAdapter.ViewHolder<MenuItem> {
   public MenuItemViewHolder(Context context, ViewGroup viewGroup) {
     super(context, viewGroup, R.layout.item_menu);
+
+    RxView.clicks(itemView.findViewById(R.id.item_special_favorite)).subscribe(new Action1<Void>() {
+      @Override
+      public void call(Void aVoid) {
+        boolean favorite = object.getFavorite();
+
+        if (favorite) {
+          Database.removeFavorite(object);
+        } else {
+          Database.addFavorite(object);
+        }
+
+        object.setFavorite(!favorite);
+      }
+    });
   }
 
   @Override
@@ -26,6 +45,7 @@ public class MenuItemViewHolder extends RecyclerViewAdapter.ViewHolder<MenuItem>
     TextView  price       = (TextView) itemView.findViewById(R.id.item_special_price);
     TextView  description = (TextView) itemView.findViewById(R.id.item_special_description);
     ImageView picture     = (ImageView) itemView.findViewById(R.id.item_special_picture);
+    CheckBox  favorite    = (CheckBox) itemView.findViewById(R.id.item_special_favorite);
 
     name.setText(object.getName());
     price.setText(object.getPrice());
@@ -34,6 +54,8 @@ public class MenuItemViewHolder extends RecyclerViewAdapter.ViewHolder<MenuItem>
     if (object.getPicture() != null && !object.getPicture().isEmpty()) {
       Picasso.with(context).load(object.getPicture()).fit().centerCrop().into(picture);
     }
+
+    favorite.setChecked(object.getFavorite());
   }
 
   public static class Factory extends RecyclerViewAdapter.Factory<MenuItemViewHolder> {
