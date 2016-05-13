64f610a66062cb3e808c2304082f2b712ac61dfe
Matthew Oltendorf <matthew@oltendorf.net>
2016-05-13 10:30:45 -0700

    Add event bus implementation

---
+++ app/src/main/java/io/mdx/app/menu/data/Bus.java
@@ -0,0 +1,21 @@
+package io.mdx.app.menu.data;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+/**
+ * Created by moltendorf on 16/5/13.
+ */
+public class Bus<T> {
+  private Subject<T, T> bus = new SerializedSubject<>(PublishSubject.<T>create());
+
+  public void send(T o) {
+    bus.onNext(o);
+  }
+
+  public Observable<T> getBus() {
+    return bus;
+  }
+}
