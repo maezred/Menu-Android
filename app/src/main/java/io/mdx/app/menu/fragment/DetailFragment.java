f34822e3dfaf443716b4470c7070919e4641039c
Matthew Oltendorf <matthew@oltendorf.net>
2016-05-23 17:19:59 -0700

    Details helper methods to create intent and bundle

--- app/src/main/java/io/mdx/app/menu/fragment/DetailFragment.java
+++ app/src/main/java/io/mdx/app/menu/fragment/DetailFragment.java
@@ -1,11 +1,22 @@
 package io.mdx.app.menu.fragment;
 
+import android.os.Bundle;
+
 import io.mdx.app.menu.R;
 
 /**
  * Created by moltendorf on 16/5/19.
  */
 public class DetailFragment extends BaseFragment {
+  private static final String ITEM_ID = "ITEM_ID";
+
+  public static Bundle createBundle(int itemId) {
+    Bundle bundle = new Bundle();
+    bundle.putInt(ITEM_ID, itemId);
+
+    return bundle;
+  }
+
   public DetailFragment() {
     super(R.layout.fragment_detail);
   }
