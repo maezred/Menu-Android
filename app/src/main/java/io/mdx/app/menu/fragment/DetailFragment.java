f9f9a1a851c8a543f133150e768187a4d4308a79
Matthew Oltendorf <matthew@oltendorf.net>
2016-05-24 14:03:55 -0700

    Bind available data to detail screen

--- app/src/main/java/io/mdx/app/menu/fragment/DetailFragment.java
+++ app/src/main/java/io/mdx/app/menu/fragment/DetailFragment.java
@@ -1,8 +1,18 @@
 package io.mdx.app.menu.fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.trello.rxlifecycle.FragmentEvent;
 
 import io.mdx.app.menu.R;
+import io.mdx.app.menu.data.Cache;
+import io.mdx.app.menu.model.MenuItem;
+import rx.functions.Action1;
 
 /**
  * Created by moltendorf on 16/5/19.
@@ -17,7 +27,50 @@ public class DetailFragment extends BaseFragment {
     return bundle;
   }
 
+  private int itemId = -1;
+
   public DetailFragment() {
     super(R.layout.fragment_detail);
+  }
+
+  @Override
+  public void setArguments(Bundle args) {
+    super.setArguments(args);
+
+    if (args != null) {
+      itemId = args.getInt(ITEM_ID, -1);
+    }
+  }
+
+  @Override
+  public void onViewCreated(final View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    if (itemId != -1) {
+      Cache.getItem(itemId)
+        .compose(this.<MenuItem>bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+        .subscribe(new Action1<MenuItem>() {
+          @Override
+          public void call(MenuItem item) {
+            TextView  name        = (TextView) view.findViewById(R.id.item_name);
+            TextView  price       = (TextView) view.findViewById(R.id.item_price);
+            TextView  description = (TextView) view.findViewById(R.id.item_description);
+            ImageView picture     = (ImageView) view.findViewById(R.id.item_picture);
+            CheckBox  favorite    = (CheckBox) view.findViewById(R.id.item_favorite);
+
+            name.setText(item.getName());
+            price.setText(item.getPrice());
+            description.setText(item.getDescription());
+
+            if (item.getPicture() != null && !item.getPicture().isEmpty()) {
+              Picasso.with(getContext()).load(item.getPicture()).fit().centerCrop().into(picture);
+            } else {
+              picture.setImageResource(android.R.color.transparent);
+            }
+
+            favorite.setChecked(item.getFavorite());
+          }
+        });
+    }
   }
 }
